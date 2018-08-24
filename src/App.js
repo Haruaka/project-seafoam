c033e5a74ba29e128dc5c46f1c9163672ef0e8d2
Shanna <ssit002@fiu.edu>
2018-08-23 22:10:23 -0400

    feat(src): style application front page

--- src/App.js
+++ src/App.js
@@ -17,13 +17,18 @@ class App extends Component {
             <div>
               <img src={profileImg} alt="Profile Img" className="profileImg"/>
             </div>
-            <div className = "textField"><h1>My Name Here</h1>
-            <h2>Graphic Designer & Front-end Developer</h2>
-            <p>
-I’m an undergraduate CS major with a passion in design. Primarily, I’m a Javascript programmer, but I know how to do HTML/CSS and Java as well. I also do designs for print materials and web or mobile mockups.
-</p>
+            <div className = "textField">
+              <h1>My Name Here</h1>
+              <h2>Graphic Designer & Front-end Developer</h2>
+              <p>
+                I’m an undergraduate CS major with a passion in design. Primarily, I’m a Javascript programmer, but I know how to do HTML/CSS and Java as well. I also do designs for print materials and web or mobile mockups.
+              </p>
+              <ul>
+                <li><i className="fab fa-github"></i></li>
+                <li><i className="fab fa-linkedin"></i></li>
+                <li><i className="fa fa-envelope"></i></li>
+              </ul>
             </div>
-
           </div>
         <div className="footer"></div>
       </div>
@@ -32,6 +37,3 @@ I’m an undergraduate CS major with a passion in design. Primarily, I’m a Jav
 }
 
 export default App;
-/*<p>
-I’m an undergraduate CS major with a passion in design. Primarily, I’m a Javascript programmer, but I know how to do HTML/CSS and Java as well. I also do designs for print materials and web or mobile mockups.
-</p>*/
